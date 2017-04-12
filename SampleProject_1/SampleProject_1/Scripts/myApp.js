1ea20bea3309d12f68f09ab9645a9666fdbb8e76
ramu <ramu10324@gmail.com>
2017-04-12 17:28:16 -0400

    Angular Script Change Adding "Failed" functionality

--- SampleProject_1/SampleProject_1/Scripts/myApp.js
+++ SampleProject_1/SampleProject_1/Scripts/myApp.js
@@ -2,9 +2,11 @@
 var myApp = angular.module('myApp', [])
                    .controller("EmpCtrl", function ($scope, $http) {
                        $scope.employees = "";
-                       $http.get("/Employee/GetEmployee")
+                       $http.get("/Employee/GetEmployee1")
                        .then (function (result) {
-                           $scope.employees = result.data;
+                           $scope.employees = result.data; // Success
                            console.log(result);
+                       }, function () {
+                           alert('Failed'); //failed
                        });
                    });
