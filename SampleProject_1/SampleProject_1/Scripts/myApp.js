824b60035e6fd848d6a42cd937acf87c16cd5b56
ramu <ramu10324@gmail.com>
2017-04-12 23:03:21 -0400

    Update in Angular Script and View

--- SampleProject_1/SampleProject_1/Scripts/myApp.js
+++ SampleProject_1/SampleProject_1/Scripts/myApp.js
@@ -1,12 +1,18 @@
 ﻿/// <reference path="angular.js" />
 var myApp = angular.module('myApp', [])
-                   .controller("EmpCtrl", function ($scope, $http) {
-                       $scope.employees = "";
-                       $http.get("/Employee/GetEmployee1")
-                       .then (function (result) {
+                   .controller("EmpCtrl", function ($scope,empservice) { //create a Employee controller 
+                       $scope.employees = null;
+                       empservice.GetEmp().then (function (result) {
                            $scope.employees = result.data; // Success
                            console.log(result);
                        }, function () {
                            alert('Failed'); //failed
                        });
-                   });
+                   })
+                   .factory('empservice', function($http){ //create and configure service using factory
+                           var fac={};
+                           fac.GetEmp=function(){
+                               return $http.get("/Employee/GetEmployee");
+                           }
+                           return fac;
+                    });
